6bd900d62a7188ddacabf25ac1f0df439aaed3b1
Idris Raja <idris.raja@gmail.com>
2025-08-19 13:18:45 -0500

    no more texliveFull, instead medium and add the needed packages

--- doc/default.nix
+++ doc/default.nix
@@ -1,12 +1,61 @@
-{ stdenvNoCC, texliveFull, python312Packages }:
+{ stdenvNoCC, pkgs, python312Packages }:
+
+let
+  tikz-uml = stdenvNoCC.mkDerivation {
+    pname = "tikz-uml";
+    version = "1.0-2016-03-29";
+    src = pkgs.fetchurl {
+      url = "https://perso.ensta-paris.fr/~kielbasi/tikzuml/var/files/src/tikzuml-v1.0-2016-03-29.tbz";
+      hash = "sha256-DLxKIMjtQBYrO5qxQAsXQpPsGtdQjmQMqHnnjEWhBdA=";
+    };
+    buildInputs = [ pkgs.bzip2 ];
+    unpackPhase = "tar -xjf $src";
+    installPhase = ''
+      mkdir -p $out/share/texmf/tex/latex/tikz-uml
+      cp tikzuml-v1.0-2016-03-29/tikz-uml.sty *.tex $out/share/texmf/tex/latex/tikz-uml/
+    '';
+  };
+
+  mytex = pkgs.texlive.combine {
+    inherit (pkgs.texlive)
+      scheme-medium
+      latex-bin
+      glossaries
+      latexmk
+      enumitem
+      tcolorbox
+      minted
+      upquote
+      biblatex
+      titlesec
+      pdfcol
+      tikzfill
+      xcolor
+      ;
+    # extra = [ tikz-uml ];
+  };
+in
 stdenvNoCC.mkDerivation {
-  name = "haskell tooling";
-  pname = "haskell tooling";
+  pname = "my-doc";
+  version = "0.1.0";
   src = ./src;
-  nativeBuildInputs = [ texliveFull python312Packages.pygments ];
+
+  buildInputs = [
+    mytex
+    python312Packages.pygments
+    pkgs.biber
+  ];
+
+  nativeBuildInputs = [
+    mytex
+    pkgs.biber
+    python312Packages.pygments
+  ];
+
   buildPhase = ''
-    latexmk 00-main.tex
+    latexmk -pdf -interaction=nonstopmode 00-main.tex
   '';
+
   installPhase = ''
     mkdir -p $out
     mv build/00-main.pdf $out/
